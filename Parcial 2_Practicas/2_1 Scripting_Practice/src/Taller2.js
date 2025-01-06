c8cd647a88557a93cfb2a0bd56525ec1ac48e9d2
DeniseRea <105187806+DeniseRea@users.noreply.github.com>
2025-01-06 10:40:06 -0500

    finish update

--- Parcial 2_Practicas/2_1 Scripting_Practice/src/Taller2.js
+++ Parcial 2_Practicas/2_1 Scripting_Practice/src/Taller2.js
@@ -13,17 +13,28 @@ function determinarMayor(a, b) {
 
 const esPar = (x) => x % 2 == 0;
 
+function validar(a,b,c){
+    let tipo=typeof(a);
+    let tipo1=typeof(b);
+    let tipo2=typeof(c);
+    if (tipo === 'number' && tipo1 === 'number' && tipo2 === 'number') {
+        return true;
+    }
+    return false;
+}
+
 const llamar_Funciones = function () {
-    let a = 5;
-    let b = 9;
-    
-    let promedio = calculaPromedio(a, b, 7);
-    let mayor = determinarMayor(a, b);
-    let par = esPar(5);
-    
-    console.log("Promedio:", promedio);
-    console.log("Mayor:", mayor);
-    console.log("Paridad de "+a + par);
+    let a=5;
+    let b=9;
+    let c=10;
+    if(validar(a,b,c)){
+        let promedio= calculaPromedio(a, b, c);
+        let mayor= determinarMayor(a, b);
+        let par= esPar(5);
+        console.log("Promedio:", promedio);
+        console.log("Mayor:", mayor);
+        console.log("Paridad de "+a + par);
+    }
 };
 
 llamar_Funciones();
